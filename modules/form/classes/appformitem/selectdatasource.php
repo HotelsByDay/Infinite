47199b94111b6a9159787f7d61737dfc3e017d09
jirkamelichar <jiri.melichar.cz@gmail.com>
2012-05-11 11:10:05 +0200

    Added conf. options 'enum_data_source' and 'enum_translations' to use ENUM options as options in the select.

--- modules/form/classes/appformitem/selectdatasource.php
+++ modules/form/classes/appformitem/selectdatasource.php
@@ -19,19 +19,50 @@ class AppFormItem_SelectDataSource extends AppFormItem_Base
      */
     protected function getValues()
     {
-        // Pokud je definovano source_model v configu, vezmeme z nej data
-        // Predpokladame ze source_model je definovan v $this->model v poli $_rel_cb
-        if (($source_model = arr::get($this->config, 'codebook', FALSE))) {
-            $filter = arr::get($this->config, 'filter', NULL);
-            
-            // Naplnime filtr z aktualniho modelu - codebook uz k nemu nema pristup
-            foreach ((array)$filter as $column => $value) {
-                if ($value == NULL) {
-                    $filter[$column] = $this->model->{$column};
-                }
+        //Loads a list of values from the ENUM field metadata.
+        //Also checks for translations in the 'enum_translations'
+        //configuration attr.
+        if (arr::get($this->config, 'enum_data_source'))
+        {
+            //list model columns - we will find enum options
+            //from this
+            $model_metadata = $this->model->list_columns();
+
+            //metadata for the attribute of this form item
+            $attr_metadata = arr::get($model_metadata, $this->attr, array());
+
+            //get the Enum options
+            $enum_options = arr::get($attr_metadata, 'options', array());
+
+            //the configuration for this form item may contain translations
+            //for each enum option
+            if (($translations = arr::get($this->config, 'enum_translations', array())))
+            {
+                return array_intersect_key($translations, array_flip($enum_options));
             }
-            return Codebook::listing($source_model, NULL, $filter);
+            else
+            {
+                //keys will be the same as valus
+                return array_combine($enum_options, $enum_options);
+            }
         }
+        else
+        {
+            // Pokud je definovano source_model v configu, vezmeme z nej data
+            // Predpokladame ze source_model je definovan v $this->model v poli $_rel_cb
+            if (($source_model = arr::get($this->config, 'codebook', FALSE))) {
+                $filter = arr::get($this->config, 'filter', NULL);
+
+                // Naplnime filtr z aktualniho modelu - codebook uz k nemu nema pristup
+                foreach ((array)$filter as $column => $value) {
+                    if ($value == NULL) {
+                        $filter[$column] = $this->model->{$column};
+                    }
+                }
+                return Codebook::listing($source_model, NULL, $filter);
+            }
+        }
+
         // Jinak vratime prazdne pole - hodnoty si pravdepodobne doplni odvozena trida
         return Array();
     }
