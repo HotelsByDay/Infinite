2cf7d10cd79dfcafbb9a25fb6a927a9bee824c17
jirkamelichar <jiri.melichar.cz@gmail.com>
2012-06-03 15:19:17 +0200

    Added default tooltip position definition into the widget/tooltip view.

--- modules/form/views/widget/tooltip.php
+++ modules/form/views/widget/tooltip.php
@@ -1,3 +1,14 @@
 <?php if ( ! empty($tooltip)):?>
+    <?php
+        if ( ! isset($tooltip_position_my))
+        {
+            $tooltip_position_my = 'left center';
+        }
+
+        if ( ! isset($tooltip_position_at))
+        {
+            $tooltip_position_at = 'right center';
+        }
+    ?>
 <span class="tooltip" position_my="<?= $tooltip_position_my;?>" position_at="<?= $tooltip_position_at;?>">?<div class="content" style="display:none"><?= $tooltip;?></div></span>
 <?php endif ?>
