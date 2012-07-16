46a09e74288b0a33b5d97c350c8f94f53b9bd122
jirkamelichar <jiri.melichar.cz@gmail.com>
2012-07-16 19:05:49 +0200

    Removed automatic adding of "clear" element after each item - it now needs to be implemented inside each separate item.

--- modules/form/views/appformitem/langstring.php
+++ modules/form/views/appformitem/langstring.php
@@ -1,43 +1,44 @@
 <div class="appformitemlangstring appformitemcontainer <?= $css?> " id="<?= $uid;?>">
 
-    <?php if ( ! empty($error_message)): ?>
-        <span class="validation_error" style="color:red;"><?= $error_message; ?></span>
+    <?php if (!empty($error_message)): ?>
+    <span class="validation_error" style="color:red;"><?= $error_message; ?></span>
     <?php endif ?>
 
-        <style type="text/css">
+    <style type="text/css">
             /* pro otestovani funkcnosti jquery pluginy */
-            div.langitem.warning select {
-                border: 1px solid red;
-                background-color: pink;
-            }
-        </style>
-        
-        
-<div class="langitems">
-    
-    <?php $i=0; foreach ($translates as $locale => $value): $i++; ?>
-            <div class="langitem group">
+        div.langitem.warning select {
+            border: 1px solid red;
+            background-color: pink;
+        }
+    </style>
 
-                <?php if ($label != ''): ?>
 
-                    <?php if ($i > 1) $final_label = $label.' '.$i; else $final_label = $label; ?>
+    <div class="langitems">
 
-                    <label for="<?= $attr.'_'.$i ?>"><?= $final_label ?></label>
+        <?php $i = 0; foreach ($translates as $locale => $value): $i++; ?>
+        <div class="langitem group">
 
-                <?php endif ?>
+            <?php if ($label != ''): ?>
 
-                <input class="langinput" type="text" id="<?= $attr.'_'.$i ?>" name="<?= $attr ?>[translates][]" value="<?= $value ?>" <?= isset($min_length) ? "minlength=\"$min_length\"" : '' ?> <?= isset($max_length) ? "maxlength=\"$max_length\"" : '' ?> />
-                <?= form::select($attr.'[locales][]', $locales, $locale) ?>
-            </div>
-    <?php endforeach; ?>
-    
-</div>
-      
-        
-        <div class="langadd">
+            <?php if ($i > 1) $final_label = $label . ' ' . $i; else $final_label = $label; ?>
+
+            <label for="<?= $attr . '_' . $i ?>"><?= $final_label ?></label>
+
+            <?php endif ?>
+
+            <input class="langinput" type="text" id="<?= $attr . '_' . $i ?>" name="<?= $attr ?>[translates][]"
+                   value="<?= $value ?>" <?= isset($min_length) ? "minlength=\"$min_length\"" : '' ?> <?= isset($max_length) ? "maxlength=\"$max_length\"" : '' ?> />
+            <?= form::select($attr . '[locales][]', $locales, $locale) ?>
+        </div>
+        <?php endforeach; ?>
+
+    </div>
+
+
+    <div class="langadd">
         <span style="display: none;"><?= __('lang.all_translations_added'); ?></span>
         <a href="javascript: ;"><?= __('lang.add_translation'); ?></a>
-        </div>
-        
-        
+    </div>
+
+
 </div>
