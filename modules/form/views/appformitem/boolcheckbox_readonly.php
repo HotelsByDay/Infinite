e5e3ee60e26a390855be81bd80e027f002006a69
Jiri Dajc <jiradajc@gmail.com>
2012-08-14 23:22:30 +0200

    boolcheckbox_readonly added, php open tag fix #726

---
+++ modules/form/views/appformitem/boolcheckbox_readonly.php
@@ -0,0 +1,16 @@
+
+<div  class="appformitemcontainer appformitemboolcheckbox <?= $css; ?>" name="<?= $attr ?>_item" id="<?= $uid; ?>">
+  <?php if (!empty($error_message)): ?>
+    <span class="validation_error"><?= $error_message; ?></span>
+  <?php endif ?>
+
+    <input type="checkbox" disabled="disabled" id="<?= $attr ?>" name="" value="1" <?php if ($value) echo 'checked="checked"'; ?> />
+    <?php if ($value): ?>
+        <input type="hidden" name="<?= $attr ?>" value="1" />
+    <?php endif; ?>
+    <label for="<?= $attr ?>"><?= $label ?></label>
+
+  <?php if (isset($hint) && !empty($hint)): ?>
+    <span class="hint"><?= $hint; ?></span>
+  <?php endif ?>
+</div>
