acc5c3a78e0aa5ce83af6489ebe64e032bae4b21
Jiri Dajc <jiradajc@gmail.com>
2012-10-05 09:31:25 +0200

    Hint added to AppFormItem_Text view \#1313

--- modules/form/views/appformitem/text.php
+++ modules/form/views/appformitem/text.php
@@ -6,14 +6,15 @@
         $name = ' name="'.$attr.'"';
     }
 ?>
-
-
-
 <?php if ( ! empty($error_message)): ?>
 <span class="validation_error"><?= $error_message;?></span>
 <?php endif ?>
 
 <label for="<?= $attr ?>"><?= $label ?></label>
 <textarea id="<?= $attr ?>"<?= $name ?><?= $disabled ?><?= isset($min_length) ? " minlength=\"$min_length\"" : '' ?> <?= isset($max_length) ? " maxlength=\"$max_length\"" : '' ?>><?= htmlspecialchars($value) ?></textarea>
+
+<?php if (isset($hint) && !empty($hint)): ?>
+    <span class="hint"><?= $hint; ?></span>
+<?php endif ?>
 
 </div>
