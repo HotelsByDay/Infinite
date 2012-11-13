b44e0c87207552d59b28d097b9f698976e09294c
Jiri Dajc <jiradajc@gmail.com>
2012-11-13 11:41:39 +0100

    afi wysiwyg updated, web class (getPageTitle) updated

--- modules/form/views/js/jquery.AppFormItemWysiwyg-init.js.php
+++ modules/form/views/js/jquery.AppFormItemWysiwyg-init.js.php
@@ -23,11 +23,17 @@ $(document).ready(function() {
         settings.buttons[settings.buttons.length] = 'image';
     }
 
+    if (typeof config.formatting_tags != 'undefined') {
+        settings.formattingTags = config.formatting_tags;
+    }
+
     //options to align text
     settings.buttons.push('|');
     settings.buttons.push('alignleft');
     settings.buttons.push('aligncenter');
     settings.buttons.push('alignright');
 
+
+    console.log(settings);
     $("#<?= $uid;?> textarea").redactor(settings);
 });
