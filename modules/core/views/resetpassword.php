1be6cd950f511c33f3e6bea6f76cced61a8dbf64
Martin Remeš <martinremes@gmail.com>
2012-12-03 11:34:12 +0100

    reset password page

--- modules/core/views/resetpassword.php
+++ modules/core/views/resetpassword.php
@@ -37,7 +37,7 @@
         <h2><?= __('resetpassword.processed');?></h2>
 
         <p>
-            <a href="<?= appurl::login_page();?>"><?= __('resetpassword.goback_to_login');?></a>
+            <a class="btn" href="<?= appurl::login_page();?>"><?= __('resetpassword.goback_to_login');?></a>
         </p>
         
         <?php else: ?>
@@ -47,13 +47,13 @@
 	<form method="post" action="<?= appurl::object_action('resetpassword', 'index');?>">
 
             <?php if (isset($validation_error)): ?>
-            <div id="err_msg">
+            <div id="err_msg" class="alert alert-error">
                 <?php echo $validation_error;?>
             </div>
             <?php endif ?>
 
             <fieldset><label for="email"><?= __('resetpassword_page.email');?></label><input id="email" type="text" name="email" value="" tabindex="1"/></fieldset>
-            <input type="submit" class="button blue" value="<?= __('resetpassword_page.reset_action');?>"/>
+            <input type="submit" class="button blue btn btn-primary" value="<?= __('resetpassword_page.reset_action');?>"/>
 
 	</form>
         <?php endif ?>
