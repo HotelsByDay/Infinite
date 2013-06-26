179745f9b9f3ac9c5b6ba9d473cea4c67c072ae1
Jiri Dajc <jiradajc@gmail.com>
2013-06-26 13:32:15 +0200

    wysiwyg images processing added

--- modules/form/classes/appformitem/wysiwyg.php
+++ modules/form/classes/appformitem/wysiwyg.php
@@ -39,6 +39,95 @@ class AppFormItem_Wysiwyg extends AppFormItem_String
 
 
     /**
+     * Tato metoda slouzi ke zpracovani formularovych udalosti.
+     *
+     * @param <int> $type Identifikator typu udalosti. Definovano konstantami
+     * AppForm::FORM_EVENT_*.
+     *
+     * @param <array> $data K obsluze udalosti mohou byt pripojeny data. Napr.
+     * k udalosti FORM_EVENT_SAVE_FAILED muze byt pripojena reference na vyjimku,
+     *  ktera zpusobyla neuspesne ulozeni zaznamu.
+     */
+    public function processFormEvent($type, $data)
+    {
+        switch($type)
+        {
+            //volano po uspesne ulozeni zaznamu
+            case AppForm::FORM_EVENT_BEFORE_SAVE:
+
+                $content = $this->getValue();
+
+                // Najdeme vsechny SRC obrazku z ukladaneho textu
+                preg_match_all('@<img.*? src="(.*?)"@', $content, $matches);
+
+                // Pridame pole nalezenych URL v danem prekladu do celkoveho pole
+                $used_urls = $matches[1];
+
+
+                // Kazdy obrazek se maze na model_name.atribut
+                $reltype = $this->getImageRelType();
+                // A zaroven na jeden zaznam
+                $relid = $this->model->pk();
+                // Precteme vsechny obrazky pro aktualni zaznam
+                $images = ORM::factory('wysiwyg_image')
+                    ->where('reltype', '=', $reltype)
+                    ->where('relid', '=', $relid)
+                    ->find_all();
+
+                // Projdeme je a pokud jejich URL neni v seznamu odkazovanych ($used_urls) pak dany obrazek odstranime
+                foreach ($images as $img)
+                {
+                    if ( ! in_array($img->getUrl(), $used_urls)) {
+                        $img->delete();
+                    }
+                }
+
+                // Projdeme text z editoru a ulozime temp obrazky a nahradime src
+                // Najdeme vsechny SRC obrazku z ukladaneho textu, ktere byly prave pridany (maji neprazdne data-tempfileid)
+                preg_match_all('@<img[^>]*? src="([^"]*)"[^>]*? data-tempfileid="([^"]*?)"@', $content, $matches);
+
+                // Pridame pole nalezenych (tempfileid => src)
+                foreach ($matches[1] as $key => $src) {
+                    $tempfileid = $matches[2][$key];
+
+                    // Vytvorime tempfile model
+                    $tempfile = ORM::factory('TempFile', $tempfileid);
+
+                    // Vytvorime model obrazku
+                    $img = ORM::factory('wysiwyg_image');
+                    // Nastavime vazby
+                    $img->reltype = $reltype;
+                    $img->relid = $relid;
+                    // Nacteme z temp modelu
+                    $img->initByTempFile($tempfile);
+                    // Ulozime
+                    $img->save();
+
+                    $replace_from = array(
+                        // replace temp src with new one
+                        'src="'.$src.'"',
+                        // remove tempid
+                        'data-tempfileid="'.$tempfileid.'"',
+                    );
+                    $replace_to = array(
+                        'src="'.$img->getUrl().'"',
+                        '',
+                    );
+
+                    // Nahradime ve virtual_value danou temp src za novou
+                    $value = str_replace($replace_from, $replace_to, $content);
+
+                    $this->setValue($value);
+                }
+                break;
+        }
+
+        // Az ted chceme ulozit virtual_value do DB
+        parent::processFormEvent($type, $data);
+    }
+
+
+    /**
      * Vrati reltype pro dane locale
      * (!) pozor, tento vypocet je implementovan i v jQuery pluginu prvku a pri zmene zde je nutne ho zmenit i tam (!)
      * @param $locale
