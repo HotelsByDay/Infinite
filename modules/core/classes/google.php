01a488a10aad397104708e00f2deb6a0d401ae40
Jiri Dajc <jiradajc@gmail.com>
2014-11-09 12:30:21 +0100

    google helper added

---
+++ modules/core/classes/google.php
@@ -0,0 +1,110 @@
+<?php
+
+class Google
+{
+    public static $results = NULL;
+
+    public static $result = array();
+
+    public static $strict = true;
+
+    public static function geocode($address, $lat=NULL, $lng=NULL)
+    {
+        $url = 'http://maps.google.com/maps/api/geocode/json';
+        $url = 'http://maps.googleapis.com/maps/api/geocode/json';
+        $params = array(
+            'address' => $address,
+            'sensor' => 'false',
+        );
+        $response = Curl::get_contents($url, $params, false);
+
+        $response = (array)@json_decode($response);
+        self::$results = (array)arr::get($response, 'results');
+
+        // If no results return 0
+        if ( ! count(self::$results)) {
+            return FALSE;
+        }
+
+
+        foreach ((array)self::$results as $result) {
+            $vp = self::getViewport((array)$result);
+            if ( ! is_null($lat) and ! is_null($lng)) {
+                // Check if given gps is inside the viewport
+                if ($vp) {
+                    if ($vp['southwest_lat'] < $lat
+                        && $vp['southwest_lng'] < $lng
+                        && $vp['northeast_lat'] > $lat
+                        && $vp['northeast_lng'] > $lng) {
+                        self::$result = $result;
+                        break;
+                    }
+                }
+            } else {
+                // use first result and stop iteration
+                self::$result = $result;
+                break;
+            }
+        }
+        return ( ! empty(self::$result));
+    }
+
+
+    public static function getLatitude()
+    {
+        $geometry = (array)arr::get((array)self::$result, 'geometry');
+        $location = (array)arr::get($geometry, 'location');
+        return arr::get($location, 'lat');
+    }
+
+    public static function getLongitude()
+    {
+        $geometry = (array)arr::get((array)self::$result, 'geometry');
+        $location = (array)arr::get($geometry, 'location');
+        return arr::get($location, 'lng');
+    }
+
+    public static function getLocationType1()
+    {
+        $result = self::$result;
+        $addr = (array)arr::get((array)$result, 'address_components');
+        $first = (array)arr::get($addr, 0);
+        $types = (array)arr::get($first, 'types');
+        return arr::get($types, 0, 'no_google_type');
+    }
+
+    public static function getLocationType2()
+    {
+        $result = self::$result;
+        $addr = (array)arr::get((array)$result, 'address_components');
+        $first = (array)arr::get($addr, 0);
+        $types = (array)arr::get($first, 'types');
+        return arr::get($types, 1, 'no_google_type');
+    }
+
+
+    public static function getViewport($result=NULL)
+    {
+        if (empty($result)) {
+            $result = (array)self::$result;
+        }
+        $addr = (array)arr::get($result, 'geometry');
+        $first = (array)arr::get($addr, 'viewport');
+        $northeast = (array)arr::get($first, 'northeast');
+        $southwest = (array)arr::get($first, 'southwest');
+
+        if ( ! isset($northeast['lat']) or $northeast['lat'] == '') {
+            return array();
+        }
+
+        return array(
+            'northeast_lat' => arr::get($northeast, 'lat'),
+            'northeast_lng' => arr::get($northeast, 'lng'),
+            'southwest_lat' => arr::get($southwest, 'lat'),
+            'southwest_lng' => arr::get($southwest, 'lng'),
+        );
+    }
+
+
+
+}
