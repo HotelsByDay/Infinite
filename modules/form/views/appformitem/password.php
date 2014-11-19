37e50e5ee8c42b8a26887205c213bc877a82293e
Jiri Dajc <jiradajc@gmail.com>
2014-11-19 13:46:42 +0100

    pass

--- modules/form/views/appformitem/password.php
+++ modules/form/views/appformitem/password.php
@@ -2,11 +2,11 @@
 
             <label for="<?= $attr; ?>-password"><?= $label;?></label>
 
-            <input <?php if (isset($placeholder) and ! empty($placeholder)) echo "placeholder=\"$placeholder\""; ?> id="<?= $attr; ?>-password" autocomplete="off"  type="password" name="<?= $attr; ?>[password]"/>
+            <input class="form-control" <?php if (isset($placeholder) and ! empty($placeholder)) echo "placeholder=\"$placeholder\""; ?> id="<?= $attr; ?>-password" autocomplete="off"  type="password" name="<?= $attr; ?>[password]"/>
 
             <label for="<?= $attr; ?>-password_confirm"><?= $label_confirm;?></label>
 
-            <input <?php if (isset($placeholder_confirm) and ! empty($placeholder_confirm)) echo "placeholder=\"$placeholder_confirm\""; ?> id="<?= $attr; ?>-password_confirm" autocomplete="off" type="password" name="<?= $attr; ?>[password_confirm]"/>
+            <input class="form-control" <?php if (isset($placeholder_confirm) and ! empty($placeholder_confirm)) echo "placeholder=\"$placeholder_confirm\""; ?> id="<?= $attr; ?>-password_confirm" autocomplete="off" type="password" name="<?= $attr; ?>[password_confirm]"/>
 
             <div class="passwords_dont_match alert alert-error" style="display:none;">
             <?= __('appformitempassword.passwords_dont_match_message'); ?>
