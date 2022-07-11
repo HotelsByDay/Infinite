dba38fa888c52d35278e90a67b7f36cc4c6a4a5e
Andrii Kuzmych <andriy.kuzjmych@gmail.com>
2022-07-11 18:21:01 +0300

    HBDK-991 Add heading and spacing options  to text editor

--- modules/form/views/js/tinymce-init.js.php
+++ modules/form/views/js/tinymce-init.js.php
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     tinymce.init({
         selector: '#<?= $uid;?> textarea',
-        plugins: 'print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable export',
+        plugins: 'autolink link table anchor lists checklist',
         toolbar_mode: 'floating',
         toolbar: 'undo redo bold italic underline strikethrough forecolor backcolor fontsize lineheight | alignleft aligncenter alignright alignjustify blocks numlist bullist checklist link table',
         menubar: '',
